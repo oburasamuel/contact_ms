d3197f7c681ab36a8cee9ff9c75a958463b6113a
Sam <oburasamuel4@gmail.com>
2024-10-28 19:28:19 +0300

    gh

--- contact.py
+++ contact.py
@@ -1,34 +1,49 @@
+# person attribute
+# add person function
+# create an empty list to store the number of people
+# Delete person function
+# loop through the people for the person to be deleted
+# search person function(an algorithm to loop through the people results)
+
+
 def add_person():
-    name = input('Name: ')
-    age = int(input('age: '))
-    email = input('email: ')
+    name = input("Name: ")
+    age = input("age: ")
+    email = input("email: ")
 
     person = {
-      'name': 'name',
-      'age': 'age',
-      'email': 'email'
+    'name': 'name',
+    'age': 'age',
+    'email': 'email'
     }
-    
+
     return person
 
+def delete_contact(people):
+    for i, person in enumerate(people):
+        print(i + 1, person["name"], "|", person["age"], "|", person["email"])
 
-def delete_people(people):
-    for i, person in enumerate(person):
-        print(i + 1, "-", person["name"], "|", person["age"], "|", person["email"])
 
+    number = input("Enter a number to be deleted: ")
 
     while True:
-        number = input("enter a number to delete: ")
-
         try:
-            if number <= 0:
-                print("Out of range")
-            elif number > people:
-                print("Out of range!")
+            number = int(number)
+            if number <= 0 or number > len(people):
+                print("Invalid range!")
+
+            else:
                 break
-        
-        except: 
-               if
+        except:
+               print("Invalid number!")
+    people.pop(number - 1)
+    print("Person deleted.")
 
 
-# def delete_contact(people):
+def search(people):
+    name = input("search for a name: ")
+    results = []
+
+    if name in people:
+        print(f"{results}")
+        results.append(name)
